7ab7688677e3e3995d9c8cbe7a04e7e5ed0a56c2
Evan Krambuhl <ekrambuhl@connectivedx.com>
2017-01-09 22:39:57 -0800

    buttoner

--- source/tags/Button.vue
+++ source/tags/Button.vue
@@ -1,13 +1,32 @@
 <script>
   import Vue from 'vue';
 
-  Vue.component('button', {
-    render: (createElement) => (
-      createElement(this.tagName, this.$slots.default)
-    ),
+  export default {
+    render: function(createElement) {
+      return createElement(
+        this.tagName,
+        {
+          class: 'Button',
+          attrs: {
+            href: this.href
+          }
+        },
+        this.$slots.default
+      )
+    },
     props: {
       tagName: { type: String, default: 'button' },
-      variant: { type: String, }
+      variant: { type: String },
+      href: { type: String }
     }
-  });
+  }
 </script>
+
+<style>
+  .Button {
+    background-color: red;
+    border: none;
+    padding: 1em;
+    font-size: 1rem;
+  }
+</style>
